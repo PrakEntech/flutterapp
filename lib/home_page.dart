f559c1d7e32d7658f310ed1fc33df8ea27499b04
Prak EnTech <62505369+PrakEntech@users.noreply.github.com>
2024-11-22 12:23:18 +0530

    Create home_page.dart

---
+++ lib/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'login_page.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to the Home Page!',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()), // Navigate back to Login
+                );
+              },
+              child: Text('Logout'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

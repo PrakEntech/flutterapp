3418c679a43463dac69df7ff024e3cad5f606c73
Prak EnTech <62505369+PrakEntech@users.noreply.github.com>
2024-11-22 12:22:07 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'home_page.dart';
+import 'login_page.dart'; // The page you want to navigate to after login
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Initialize Firebase
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Firebase Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(), // The login page
+    );
+  }
+}
